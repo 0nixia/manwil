20ba2beb4ebfee6c4e11ee83295e6264cf06bf1e
bianca-soliz-jalafund <rbiancasoliz@gmail.com>
2026-06-03 12:09:39 -0400

    [FIX] The user is not an employee

--- siat_module/models/ir_menu.py
+++ siat_module/models/ir_menu.py
@@ -8,7 +8,7 @@ class Menu(models.Model):
   @tools.ormcache('frozenset(self.env.user.groups_id.ids)', 'debug')
   def _visible_menu_ids(self, debug=False):
     menus = super(Menu, self)._visible_menu_ids(debug)
-    config = self.env['ir.config_parameter'].get_param('siat_config.mode_siat')
+    config = self.env['ir.config_parameter'].sudo().get_param('siat_config.mode_siat')
     if config != '1':
       menu_item_id = self.env.ref('siat_module.pending_invoices').id
       menus.discard(menu_item_id)
